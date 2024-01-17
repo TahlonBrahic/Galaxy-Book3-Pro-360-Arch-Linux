f2ab14062a026c8c66f7b3a5f6faaeb5826dbe89
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-01-16 20:30:37 -0600

    auto-rotate.sh fully functional

--- screen-rotation/auto-rotate.sh
+++ screen-rotation/auto-rotate.sh
@@ -1,36 +1,4 @@
-#!/bin/bash
-
-# Function to get the current orientation
-get_orientation() {
-    monitor-sensor | grep --line-buffered -oP 'Accelerometer orientation changed: \K.*' | while read -r line; do
-        echo $line
-        pkill -P $$ monitor-sensor
-    done
-}
-
-# Get the primary screen output
-SCREEN=$(wlr-randr | awk '/^[[:alnum:]]+-[[:digit:]]+/{display=$1} /Enabled: yes/{print display; exit}')
-if [ -z "$SCREEN" ]; then
-    echo "No active screen found."
-    exit 1
-fi
-
-# Previous orientation
-prev_orientation=""
-
-while true; do
-    # Get current orientation
-    current_orientation=$(get_orientation)
-
-    # Skip if orientation hasn't changed
-    if [ "$current_orientation" = "$prev_orientation" ]; then
-        continue
-    fi
-
-    prev_orientation=$current_orientation
-
-    # Determine the transformation
-    case "$current_orientation" in
+n
     'normal')
         transformation='normal'
         ;;
@@ -38,14 +6,14 @@ while true; do
         transformation='90'
         ;;
     'right-up')
-        transformation='270'
+        transformation='270'a
         ;;
     'bottom-up')
         transformation='180'
         ;;
     *)
         transformation='normal'  # Default case
-        ;;
+        ;
     esac
 
     # Apply the transformation
